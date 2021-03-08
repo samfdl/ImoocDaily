080c97a046114701c738c798805e35ce8a72cb09
冯德林 <samfdl@163.com>
2021-03-08 10:38:38 +0800

    wifi热点黑名单 加入弹出框

---
+++ src/com/android/settings/wifi/tether/WifiTetherConnectedDevicesPreferenceController.java
@@ -0,0 +1,179 @@
+/*
+ * Copyright (C) 2017 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.wifi.tether;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.net.wifi.WifiConfiguration;
+import android.net.wifi.WifiDevice;
+import android.net.wifi.WifiManager;
+import android.os.Build;
+import androidx.preference.SwitchPreference;
+import androidx.preference.Preference;
+import androidx.preference.PreferenceCategory;
+import android.util.Log;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.android.settings.R;
+import com.android.settingslib.core.lifecycle.events.OnStart;
+import com.android.settingslib.core.lifecycle.events.OnStop;
+
+public class WifiTetherConnectedDevicesPreferenceController extends WifiTetherBasePreferenceController
+        implements OnStart, OnStop {
+
+    private static final String PREF_KEY = "ap_device_list";
+    private static final String TAG = "WifiTetherConnectedDevicesPreferenceController";
+    private static final boolean DBG = false;
+
+    private List<String> mConnectedNameList = new ArrayList<String>();
+    private List<String> mConnectedAddressList = new ArrayList<String>();
+    private List<WifiDevice> mTetherConnectedDeviceList = new ArrayList<WifiDevice>();
+    private PreferenceCategory mWifiApListPrefCategory;
+    private Context mContext;
+    private WifiTetherSoftApManager mWifiTetherSoftApManager;
+    private WifiManager mWifiManager;
+
+    public WifiTetherConnectedDevicesPreferenceController(Context context,
+            OnTetherConfigUpdateListener listener) {
+        super(context, listener);
+        mContext = context;
+        mWifiManager = (WifiManager) mContext.getSystemService(WifiManager.class);
+        initWifiTetherSoftApManager();
+    }
+
+    @Override
+    public void onStart() {
+        if (mPreference != null) {
+            if (mWifiTetherSoftApManager != null) {
+                if (DBG) Log.d(TAG, "registerSoftApCallback");
+                mWifiTetherSoftApManager.registerSoftApCallback();
+            }
+        }
+    }
+
+    @Override
+    public void onStop() {
+        if (mPreference != null) {
+            if (mWifiTetherSoftApManager != null) {
+                if (DBG) Log.d(TAG, "unRegisterSoftApCallback");
+                mWifiTetherSoftApManager.unRegisterSoftApCallback();
+            }
+        }
+    }
+
+    private void initWifiTetherSoftApManager() {
+        mWifiTetherSoftApManager = new WifiTetherSoftApManager(mWifiManager,
+                new WifiTetherSoftApManager.WifiTetherSoftApCallback() {
+                    @Override
+                    public void onStateChanged(int state, int failureReason) {
+                        if (DBG) Log.d(TAG, "onStateChanged: " +state);
+                        updateConnectedDevices();
+                    }
+
+                    @Override
+                    public void onNumClientsChanged(int numClients) {
+                        if (DBG) Log.d(TAG, "onNumClientsChanged: " +numClients);
+                        try {
+                            Thread.sleep(30);
+                        } catch (InterruptedException e) {
+                            Log.e(TAG, ""+e);
+                        }
+                        updateConnectedDevices();
+                    }
+                });
+    }
+
+    @Override
+    public void updateDisplay() {
+        mWifiApListPrefCategory = (PreferenceCategory) mPreference;
+        updateConnectedDevices();
+    }
+
+    @Override
+    public String getPreferenceKey() {
+        return PREF_KEY;
+    }
+
+    @Override
+    public boolean isAvailable() {
+        return true;
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        return true;
+    }
+
+    private void updateConnectedDevices() {
+        mTetherConnectedDeviceList = mWifiManager.getConnectedStations();
+        if (mTetherConnectedDeviceList == null || mTetherConnectedDeviceList.size() == 0) {
+            if (DBG) Log.d(TAG, "ConnectedCount = 0");
+            mWifiApListPrefCategory.removeAll();
+            return;
+        }
+        mConnectedNameList.clear();
+        mConnectedAddressList.clear();
+        for (int i = 0; i < mTetherConnectedDeviceList.size(); i++) {
+            WifiDevice device = mTetherConnectedDeviceList.get(i);
+            mConnectedNameList.add(device.deviceName != null ? device.deviceName : "");
+            mConnectedAddressList.add(device.deviceIpAddress+"\n"+device.deviceAddress);
+        }
+        mWifiApListPrefCategory.removeAll();
+        for (int index = 0; index < mConnectedAddressList.size(); ++index) {
+            if (DBG) Log.d(TAG, "Connected device address - " + mConnectedAddressList.get(index));
+            Preference pref = new Preference(mContext);
+            if (mConnectedNameList != null && !mConnectedNameList.get(index).isEmpty()) {
+                if (DBG) Log.d(TAG, "Connected device name - " + mConnectedNameList.get(index));
+                pref.setTitle(mConnectedNameList.get(index));
+            } else {
+                pref.setTitle(R.string.wifiap_default_device_name);
+            }
+            pref.setSummary(mConnectedAddressList.get(index));
+            pref.setOnPreferenceClickListener(
+                    preference -> {
+                        showDisconnectDialog();
+                        return true;
+                    });
+            mWifiApListPrefCategory.addPreference(pref);
+        }
+    }
+
+    private void showDisconnectDialog(){
+        final AlertDialog.Builder disconnectDialog = new AlertDialog.Builder(mContext);
+        disconnectDialog.setTitle("Disconnect now?");
+        disconnectDialog.setMessage("This action will disconnect \"00:01:f5:de:f4:c8\" from this device and add it to blocklist");
+        disconnectDialog.setPositiveButton("OK",
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        //...To-do
+                    }
+                });
+        disconnectDialog.setNegativeButton("Cancel",
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        //...To-do
+                    }
+                });
+        // 显示
+        disconnectDialog.show();
+    }
+}
